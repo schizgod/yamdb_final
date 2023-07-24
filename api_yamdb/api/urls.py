45e98379a0bed910e9d22f6c690192202f27e9ff
schizgod <schizgod@gmail.com>
2023-07-24 03:05:08 +0300

    static added

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,45 +1,7 @@
 from django.urls import include, path
-from rest_framework.routers import SimpleRouter
 
-from .views import (APIGetToken, APISignup, CategoryViewSet, CommentViewSet,
-                    GenreViewSet, ReviewViewSet, TitleViewSet, UsersViewSet)
-
-app_name = 'api'
-
-router = SimpleRouter()
-router.register(
-    r'titles/(?P<title_id>\d+)/reviews',
-    ReviewViewSet,
-    basename='reviews'
-)
-router.register(
-    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentViewSet,
-    basename='comments'
-)
-router.register(
-    'users',
-    UsersViewSet,
-    basename='users'
-)
-router.register(
-    'categories',
-    CategoryViewSet,
-    basename='сategories'
-)
-router.register(
-    'titles',
-    TitleViewSet,
-    basename='titles'
-)
-router.register(
-    'genres',
-    GenreViewSet,
-    basename='genres'
-)
+from .v1 import urls as v1_urls
 
 urlpatterns = [
-    path('v1/auth/token/', APIGetToken.as_view(), name='get_token'),
-    path('v1/', include(router.urls)),
-    path('v1/auth/signup/', APISignup.as_view(), name='signup'),
+    path('v1/', include(v1_urls))
 ]
---
+++ api_yamdb/api/v1/urls.py
@@ -0,0 +1,27 @@
+from api.v1.views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                          ReviewViewSet, TitleViewSet, UserViewSet,
+                          obtain_token, signup)
+from django.urls import include, path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+router.register('categories', CategoryViewSet, basename='category')
+router.register('genres', GenreViewSet, basename='genre')
+router.register('titles', TitleViewSet, basename='title')
+router.register('users', UserViewSet, basename='user')
+router.register(r'titles/(?P<title_id>\d+)/reviews',
+                ReviewViewSet, basename='reviews')
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='comments')
+
+auth_patterns = [
+    path('signup/', signup, name='signup'),
+    path('token/', obtain_token, name='token')
+]
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/', include(auth_patterns))
+]
